e942c9d78ecd99c6b0701cd5bc04df9a51abdb8d
balltwance <danupolg@gmail.com>
2025-06-05 15:14:31 +0700

    อัพเดท routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,27 +2,30 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
-
- {
+  {
     path: '',
     redirectTo: 'login',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'login',
-    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
   },
   {
     path: 'register',
-    loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent)
+    loadComponent: () =>
+      import('./register/register.component').then((m) => m.RegisterComponent),
   },
   {
-    path: 'main',
+    path: '',
     component: MainLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./home/home.component').then((m) => m.HomeComponent),
+      },
+    ],
   },
-  
-
-
-  
-
 ];
