6eec6fd72a70f004ab5d0d775dab80265941f590
balltwance <danupolg@gmail.com>
2025-06-05 14:30:55 +0700

    create main-layout, header, add tailwind
    create main-layout coponent , header component , add tailwindcss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./home/home.component').then((m) => m.HomeComponent),
+      },
+    ],
+  },
+];
---
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../header/header.component';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [RouterOutlet, CommonModule, HeaderComponent],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.scss',
+})
+export class MainLayoutComponent {}
