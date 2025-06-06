bf537e9cf1c543fb340a5c29aa35391e2963809d
balltwance <danupolg@gmail.com>
2025-06-06 16:59:51 +0700

    add font in angular material

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
