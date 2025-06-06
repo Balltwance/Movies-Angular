e74fc1dab0dd95c393e06e01e449f1974d0696dc
balltwance <danupolg@gmail.com>
2025-06-06 14:02:44 +0700

    สร้าง footer component

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-footer',
+  imports: [RouterModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {
+  footerLinks = [
+    { name: 'คำถามที่พบบ่อย', path: '' },
+    { name: 'ศูนย์ช่วยเหลือ', path: '' },
+    { name: 'ข้อกำหนดการใช้งาน', path: '' },
+    { name: 'ความเป็นส่วนตัว', path: '' },
+    { name: 'การตั้งค่าคุกกี้', path: '' },
+    { name: 'ข้อมูลบริษัท', path: '' },
+  ];
+}
--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../header/header.component';
+import { FooterComponent } from '../../footer/footer.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, CommonModule, HeaderComponent],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
